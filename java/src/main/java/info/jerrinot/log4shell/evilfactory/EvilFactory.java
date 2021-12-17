c36fa79b2ad596dcf1d957dc0501092ed624fd1d
Andreas Motl <amo@backdoor-collective.org>
2021-12-17 21:20:46 +0100

    Add log messages to `EvilFactory` implementation

--- java/src/main/java/info/jerrinot/log4shell/evilfactory/EvilFactory.java
+++ java/src/main/java/info/jerrinot/log4shell/evilfactory/EvilFactory.java
@@ -6,8 +6,20 @@ import javax.naming.spi.ObjectFactory;
 import java.util.Hashtable;
 
 public class EvilFactory implements ObjectFactory {
+    private static final String tag = "Log4Shell demo";
+
+    static void log(String msg) {
+        System.out.println("[" + tag + "] " + msg);
+    }
+
+    static void error(String msg) {
+        System.err.println("[" + tag + "] " + msg);
+    }
+
     @Override
     public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) {
+        log("Log4Shell demo entrypoint");
+        error("Log4Shell demo entrypoint");
         return "totally pwned!";
     }
 }
